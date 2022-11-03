cbbbfd4ebcd2d77c890140f33949222b1324d799
hasan <hassanzohdy@gmail.com>
2022-11-03 08:22:00 +0200

    Added database configurations into .env

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,11 @@
+import { env } from "@mongez/dotenv";
+
 const databaseConfigurations = {
-  host: "localhost",
-  port: 27017,
-  username: "root",
-  password: "root",
-  name: "ninjaNode",
+  host: env("DB_HOST", "localhost"),
+  port: env("DB_PORT", 27017),
+  username: env("DB_USERNAME"),
+  password: env("DB_PASSWORD"),
+  name: env("DB_NAME"),
 };
 
 export default databaseConfigurations;
